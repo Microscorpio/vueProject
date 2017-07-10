28b6d7f18da4b58e6d7193d2121c50e9cf25f350
Empty <10419925@qq.com>
2017-07-10 23:22:08 +0800

    update code about header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
   <div>
-    <img src="./assets/logo.png">
-    <router-link to="/goods">goods</router-link>
-    <router-link to="/ratings">ratings</router-link>
-    <router-link to="/seller">seller</router-link>
+    <v-header :seller="seller"></v-header>
+    <div class="tab-list border-1px">
+      <div class="tab-item">
+        <router-link to="/goods">goods</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">ratings</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">seller</router-link>
+      </div>
+    </div>
     <transition name="fade" mode="out-in">
       <router-view></router-view>
     </transition>
@@ -11,16 +19,54 @@
 </template>
 
 <script>
+import header from './components/header/header';
+
+const ERR_OK = 0;
 export default {
+  data() {
+    return {
+      seller: {}
+    };
+  },
+  created() {
+    this.$http.get('/api/seller').then((response) => {
+      response = response.body;
+      if (response.errno === ERR_OK) {
+        this.seller = response.data;
+      }
+    });
+  },
+  components: {
+    'v-header': header
+  }
 };
 </script>
 
 <style lang="scss">
+@import "./common/sass/base.scss";
 .fade-enter-active, .fade-leave-active {
-  transition: opacity .5s ease;
+  transition: opacity .3s ease;
 }
 .fade-enter, .fade-leave-active {
   opacity: 0
 }
-
+.tab-list{
+  width: 100%;
+  height: 40px;
+  line-height: 40px;
+  display: flex;
+  @include border-1px(rgba(7, 17, 27, 0.1));
+  .tab-item{
+    flex: 1;
+    text-align: center;
+    a{
+      display: block;
+      font-size: 14px;
+      color: rgb(77, 85, 93);
+      &.active{
+        color: rgb(240, 20, 20);
+      }
+    }
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 import Vue from 'vue';
 import App from './App';
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 import seller from './components/seller/seller';
 import goods from './components/goods/goods';
 import ratings from './components/ratings/ratings';
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
 
 // Vue.config.productionTip = false
-
 const routes = [ {
   path: '/',
   redirect: '/goods'
@@ -24,11 +25,12 @@ const routes = [ {
 } ];
 
 const router = new VueRouter({
+  linkActiveClass: 'active',
   routes
 });
 
 /* eslint-disable no-new */
 new Vue({
-  router,
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app');
